3fe535fa7bc734fe2d49a509d712d0fbc9633e63
Will Dower <wdower@mitre.org>
2022-10-13 16:55:16 -0400

    added delta test command to npm scripts, changed test command in delta.test.ts
    Signed-off-by: Will Dower <wdower@mitre.org>

--- test/commands/generate/delta.test.ts
+++ test/commands/generate/delta.test.ts
@@ -2,24 +2,22 @@ import {expect, test} from '@oclif/test'
 import tmp from 'tmp'
 import path from 'path'
 import fs from 'fs'
-import fse from 'fs-extra'
+import fse from 'fse'
 import {execSync} from 'child_process'
 import { inspecControlToRubyCode } from '../../../src/utils/xccdf2inspec'
 
 describe('Test generate delta', () => {
   const tmpobj = tmp.dirSync({unsafeCleanup: true})
-  fse.copySync(path.resolve('./test/sample_data/delta-test/profile'), tmpobj.name);
+  fse.copydir(path.resolve('./test/sample_data/delta-test/profile'), tmpobj.name);
   
   test
+  .command(['generate delta', '-i', path.resolve(tmpobj.name) + ' ' + path.resolve(`${tmpobj.name}/profile.json`), '-r', path.resolve(`${tmpobj.name}/report.md`)])
   .it('report is generated for a given InSpec profile update', () => {
-      execSync(
-          `node bin/run generate delta -i ${tmpobj.name} ${tmpobj.name}/profile.json -r ${tmpobj.name}/report.md`,
-          )
-          
-          const files = fs.readdirSync(tmpobj.name);
-          for (const file of files) {
-            console.log(file)
-          }
+
+          // const files = fs.readdirSync(tmpobj.name);
+          // for (const file of files) {
+          //   console.log(file)
+          // }
     // const test = JSON.parse(fs.readFileSync(`${tmpobj.name}/asfftest/CIS AWS Foundations Benchmark v1.2.0.json`, 'utf8'))
     // const sample = JSON.parse(fs.readFileSync(path.resolve('./test/sample_data/asff/asff-cis_aws-foundations_benchmark_v1.2.0-hdf.json'), 'utf8'))
     expect((1)).to.eql((1))
